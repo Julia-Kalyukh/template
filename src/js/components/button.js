4c8e521ca367cc9b57c40cd1620568f5fc3f3160
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2022-10-20 23:34:37 +0300

    Добавлена кнопка "Показать все"

---
+++ src/js/components/button.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  function btnShowAll (btn, item) {
+
+    if (item.length > 2) {
+      btn.style.display = 'block';
+    }
+
+    btn.addEventListener('click', function () {
+
+      if (this.innerText === 'Показать все') {
+        this.innerText = 'Скрыть';
+
+        item.forEach(i => {
+          i.style.display = 'flex';
+        });
+
+      } else {
+        this.innerText = 'Показать все';
+
+        item.forEach(i => {
+          i.style.display = 'none';
+        });
+      }
+    });
+  }
+
+  btnShowAll(document.querySelector('.js-show-contacts'), document.querySelectorAll('.js-item-block'));
+
+});
